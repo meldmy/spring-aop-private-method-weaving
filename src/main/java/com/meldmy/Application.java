82c70a70c7dfb3121623e542a935dad663dfd7be
Dmytro Melnychuk <melnichukdv@gmail.com>
2018-09-19 23:49:34 +0200

    Add Spring command line runner to call tested logic

--- src/main/java/com/meldmy/Application.java
+++ src/main/java/com/meldmy/Application.java
@@ -1,5 +1,8 @@
 package com.meldmy;
 
+import com.meldmy.sample.some_logic.LogicUnderWeaving;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Dmytro Melnychuk
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private LogicUnderWeaving logicUnderWeaving;
+
+    @Override
+    public void run(String... args) {
+        logicUnderWeaving.foo();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
