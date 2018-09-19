0beedfa45451ff0dea5f8ef82833d8a5129be6cc
Dmytro Melnychuk <melnichukdv@gmail.com>
2018-09-19 23:45:24 +0200

    Add SpringBoot application

---
+++ src/main/java/com/meldmy/Application.java
@@ -0,0 +1,15 @@
+package com.meldmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dmytro Melnychuk
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
